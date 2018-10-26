8358fe3cb47445fac7652e4c64634093a39eac13
andmorefine <andmorefine@gmail.com>
2018-10-27 07:32:52 +0900

    commit

--- config/puma.rb
+++ config/puma.rb
@@ -23,6 +23,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -33,3 +35,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+
