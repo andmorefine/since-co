2f89c5d6d8987e972bee44da5fa6df9afad280cf
andmorefine <andmorefine@gmail.com>
2019-02-17 22:38:26 +0900

    add create_issue

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -11,6 +11,7 @@ class ContactController < ApplicationController
     @contact.save
     message = "[info][title]モゲーーー[/title]お問い合わせがあったよ [/info]"
     Chatwork::MessageService.new(room_id: CHATWORK_ROOM, body: message).create
+    Github::IssueService.new(title: contact_params[:subject], body: contact_params[:body]).create
     redirect_to thanks_contact_index_path
   end
 
---
+++ app/services/github/issue_service.rb
@@ -0,0 +1,20 @@
+require 'octokit'
+
+class Github::IssueService
+
+  def initialize(params = {})
+    @title = params[:title].present? ? params[:title] : "title"
+    @body = params[:body].present? ? params[:body] : "body"
+  end
+
+  def create
+    client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+
+    repo = "andmorefine/since-co"
+    title = @title
+    body = @body
+    options = {labels: 'contact'}
+
+    res = client.create_issue(repo, title, body, options)
+  end
+end
