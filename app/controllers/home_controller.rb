21c18bd8ea6b870b3984f911957ae47c47138163
andmorefine <andmorefine@gmail.com>
2019-09-08 05:17:27 +0900

    home update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@
 class HomeController < ApplicationController
   def index
     @proverb_count = Proverb.all.count
+    @proverb_radom = Proverb.active.image_active.sample(3).shuffle
     @twitters = Twitter::ClientService.new.fetch
   end
 end
