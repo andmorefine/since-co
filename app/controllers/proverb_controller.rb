7650ba53dfec4470140b954e0b318623b2995947
andmorefine <andmorefine@gmail.com>
2019-09-09 23:31:18 +0900

    update proverb_synonyms

--- app/controllers/proverb_controller.rb
+++ app/controllers/proverb_controller.rb
@@ -20,21 +20,14 @@ class ProverbController < ApplicationController
     @proverb = Proverb.find_by_id(params[:id])
     return redirect_to proverb_index_path if @proverb.nil?
 
+    @proverb_synonyms = []
+    @proverb.proverb_synonyms.active.map do |synonym|
+      @proverb_synonyms.push(Proverb.find_by_id(synonym.proverb_synonym_id))
+    end
     @others = Proverb.where(alphabetal_id: @proverb.alphabetal_id).where(delete_flag: false)
     @proverb_all = Proverb.all.count
     @proverb_prev = @proverb.id - 1 unless @proverb.id == 1
     @proverb_next = @proverb.id + 1 unless @proverb.id == @proverb_all
-
-    versions = Proverb.find(params[:id]).versions.where(event: :update).sort.reverse
-    @versions = []
-    versions.each do |version|
-      @versions.push(
-        id: version.id,
-        object_changes: YAML.safe_load(version.object_changes),
-        whodunnit: version.whodunnit,
-        created_at: version.created_at.strftime('%Y年%m月%d日 %H:%M')
-      )
-    end
   end
 
   def edit; end
