cfadd874cccdb69f3c84cc1bee72d7b5793076e1
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2022-05-23 22:12:38 +0900

    update master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @proverb_count = Proverb.all.count
     @proverb_radom = Proverb.active.image_active.sample(3).shuffle
-    @proverb_image = Proverb.active.image_active.sample(1).first.image
+    @proverb_image = Proverb.active.image_active.sample(1).first&.image
     @twitters = Twitter::ClientService.new.fetch
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -110,9 +110,9 @@ ActiveRecord::Schema.define(version: 2019_09_10_141446) do
     t.integer "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object", limit: 4294967295
+    t.text "object", size: :long
     t.datetime "created_at"
-    t.text "object_changes", limit: 4294967295
+    t.text "object_changes", size: :long
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
